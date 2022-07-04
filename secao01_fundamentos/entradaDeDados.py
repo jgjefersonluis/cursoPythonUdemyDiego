2b25fc0a024df0020ec393e377ff2a7156be1426
Jeferson Luis <jgjefersonluis@gmail.com>
2022-07-03 23:56:37 -0300

    input -> entrada de dados

---
+++ secao01_fundamentos/entradaDeDados.py
@@ -0,0 +1,14 @@
+'''
+Entrada de dados pelo usuário
+Curso de Python
+'''
+
+# Recebendo os dados do usuário com a função input()
+nome = input('Qual o seu nome?')
+idade = input('Qual a sua idade?')
+peso = input('Qual o seu peso?')
+
+# Exibindo os resultados na tela
+print('n\O nome digitado foi: ', nome)
+print('A idade digitada foi: ', idade)
+print('O peso digitado foi: ', peso)
---
+++ secao01_fundamentos/variaveis.py
@@ -0,0 +1,18 @@
+'''
+Utilizando variáveis
+Curso de Python
+'''
+
+# criando uma variável do tipo (string) e fazendo ela receber um valor
+nome = 'Regina'
+
+# criando variáveis com números e atribuindo valores
+idade = 51
+peso = 116
+
+# Exibindo na tela as variáveis
+print('Nome => ' + nome)
+print('Idade => ', idade)
+print('Peso => ',  peso)
+
+print(nome,',', idade,',', peso)
