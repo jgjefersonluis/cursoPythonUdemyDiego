0e4ad8452ce9bcd3d9ab6a63369dde7ba92899ce
Jeferson Luis <jgjefersonluis@gmail.com>
2022-07-03 23:39:06 -0300

    comentários

---
+++ secao01_fundamentos/comentarios.py
@@ -0,0 +1,34 @@
+'''
+Utilizando os comentários nos scripts Python
+Curso de Python
+
+Comentários de uma linha começam com # (hastag)
+Comentários de várias linhas são cercados por 3 aspas simples
+'''
+
+# ---
+
+'''
+Comentário
+de
+várias
+linhas
+'''
+
+# Comentário de uma linha
+
+# Exibindo mensagens, ou seja, textos - string
+print('Mensagem na tela')
+print("Outra mensagem")
+
+# Exibindo números inteiros
+print(3)
+
+# Exibindo uma soma
+print(3+5)
+
+# Agregando duas strings
+print('3+5' + '=8')
+
+# Agregando uma string com um número
+print('9857+4745')
