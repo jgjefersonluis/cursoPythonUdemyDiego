8ad60c7f9fcb89fc3bbe0cc486e6ebca59b20607
Jeferson Luis <jgjefersonluis@gmail.com>
2022-08-03 14:26:02 -0300

    install pygame - script que abra e execute u arquivo mp3

---
+++ secao01_fundamentos/exercicios/Exercicio_D_04.py
@@ -0,0 +1,16 @@
+'''
+Exercício 4
+---
+Faça um script que abra e execute um arquivo MP3.
+Módulo: pygame
+'''
+# Importando o PyGame
+import pygame
+
+# Inicializando o PyGame
+pygame.init()
+
+# Carregando o arquivo MP3 e executando
+pygame.mixer.music.load('musica.mp3')
+pygame.mixer.music.play()
+pygame.event.wait()
