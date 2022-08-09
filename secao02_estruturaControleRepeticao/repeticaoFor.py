fbbb75fdf05568327a9b5225d711979c8075cf9f
Jeferson Luis <jgjefersonluis@gmail.com>
2022-08-09 18:20:18 -0300

    repetições laço For

---
+++ secao02_estruturaControleRepeticao/repeticaoFor.py
@@ -0,0 +1,26 @@
+'''
+Estruturas de Repetição - For
+'''
+
+# Mostrando de 1 até 10
+for num in range(1, 11):
+    print('Número{}'.format(num))
+
+# Mostrando frases no terminal
+for num in range(0, 5):
+    print('Curso de Python')
+    print('Estrutura de repetição')
+
+# Mostrando de 10 até 1 no terminal, utilizando o passo -1
+for num in range(10, 0, -1):
+    print('Número{}'.format(num))
+
+# Mostrando números pares, utilizando o passo 2
+for num in range(0, 11, 2):
+    print('Número {}'.format(num))
+
+
+
+
+
+
---
+++ secao02_estruturaControleRepeticao/repetiçãoForTabuada.py
@@ -0,0 +1,16 @@
+print('1 x {} = {}'.format(6, 1 * 6))
+print('2 x {} = {}'.format(6, 2 * 6))
+print('3 x {} = {}'.format(6, 3 * 6))
+print('4 x {} = {}'.format(6, 4 * 6))
+print('5 x {} = {}'.format(6, 5 * 6))
+print('6 x {} = {}'.format(6, 6 * 6))
+print('7 x {} = {}'.format(6, 7 * 6))
+print('8 x {} = {}'.format(6, 8 * 6))
+print('9 x {} = {}'.format(6, 9 * 6))
+print('10 x {} = {}'.format(6, 10 * 6))
+
+print('')
+
+tabuada = 6
+for num in range(1,11):
+    print('{} x {} = {}'.format(num, tabuada, num * tabuada))
