0311382fdeae25f2e3e31513dd11f41f8286dca7
Jeferson Luis <jgjefersonluis@gmail.com>
2022-08-19 00:45:05 -0300

    estrutura de repetição números impares entre 1 e 50

---
+++ secao02_estruturaControleRepeticao/Exercicios/Exercicios_I_02.py
@@ -0,0 +1,10 @@
+'''
+Exercícios de Python -> Estrutura de Repetição
+Exercício 2
+---
+Exiba na tela todos os números ímpares entre 1 e 50.
+'''
+
+print('Números ímpares:')
+for i in range(1, 51, 2):
+    print(i)
